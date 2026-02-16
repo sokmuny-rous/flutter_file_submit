8b13da3dbe114a5e39e2304bb055625fef0062ac
Your Name <your.email@example.com>
2026-02-16 15:18:38 +0700

    Change subject

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -61,7 +61,7 @@ class _HomeViewState extends State<HomeView> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
                     Text(
-                      'Subject',
+                      'Subject to confess this year',
                       style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
